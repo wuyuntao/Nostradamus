018638375a9cf79e9c1ebc1f56cc91fd40557121
Wu Yuntao <wyt.brandon@gmail.com>
2016-10-17 16:06:37 +0800

    1, Generate FlatBuffers code in one file

--- Scripts/FlatBuffersCodeGen.ps1
+++ Scripts/FlatBuffersCodeGen.ps1
@@ -1,4 +1,4 @@
 $SolutionDir = "$PSScriptRoot\.."
 $ProjectDir = "$SolutionDir\Nostradamus"
 
-& $SolutionDir\Lib\FlatBuffers\flatc.exe -n -o $ProjectDir\GeneratedCode $ProjectDir\Nostradamus.fbs
+& $SolutionDir\Lib\FlatBuffers\flatc.exe -n --gen-onefile -o $ProjectDir\Networking $ProjectDir\Networking\Schema.fbs
--- Scripts/ReleaseUnitySource.ps1
+++ Scripts/ReleaseUnitySource.ps1
@@ -2,7 +2,7 @@ $SolutionDir = "$PSScriptRoot\.."
 $SourceDir = "$SolutionDir\Nostradamus"
 $TargetDir = "$SolutionDir\NotradamusUnity\Assets\Notradamus\Scripts"
 
-$SourceSubDirs = ("Client", "Core", "Examples", "GeneratedCode", "Networking", "Physics", "Server", "Utils")
+$SourceSubDirs = ("Client", "Core", "Examples", "Networking", "Physics", "Server", "Utils")
 
 foreach ($SubDir in $SourceSubDirs) {
     cp -Force -Recurse $SourceDir\$SubDir $TargetDir
